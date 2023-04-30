c59e11b0cfe16ad0ad9567c94a175f5f21a435c7
CARLOS REINALDO GARCIA <kalliche16@gmail.com>
2023-04-29 22:20:44 -0500

    ajustar tamaño del canvas de lados iguales

---
+++ game.js
@@ -0,0 +1,41 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+
+  const elementsSize = canvasSize / 10;
+
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
+
+  // window.innerHeight
+  // window.innerWidth
+
+  // game.fillRect(0,50,100,100);
+  // game.clearRect(50,50,50,50);
+  // game.clearRect()
+  // game.clearRect(0,0,50,50);
+
+  // game.font = '25px Verdana'
+  // game.fillStyle = 'purple';
+  // game.textAlign = 'center';
+  // game.fillText('Platzi', 25, 25);
+}
