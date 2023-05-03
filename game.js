3f8d91d4b4cefdcd3bc8581633160ecdad4f07e3
CARLOS REINALDO GARCIA <kalliche16@gmail.com>
2023-05-03 08:58:08 -0500

    movimmientos de jugador moveUp

--- game.js
+++ game.js
@@ -1,11 +1,23 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
 
-function startGame() {
-  let canvasSize;
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+// responsive cambas
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
@@ -15,27 +27,95 @@ function startGame() {
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
 
-  const elementsSize = canvasSize / 10;
+  elementsSize = canvasSize / 10;
 
+  startGame();
+}
+
+function startGame() {
   console.log({ canvasSize, elementsSize });
 
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize, elementsSize * i);
-  }
+  const map = maps[0];
+  const mapRows = map.trim().split('\n');
+  const mapRowCols = mapRows.map(row => row.trim().split(''));
+  //console.log({map, mapRows, mapRowCols});
 
-  // window.innerHeight
-  // window.innerWidth
+  // recorrer el array y remplaza los for de abajo
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
 
-  // game.fillRect(0,50,100,100);
-  // game.clearRect(50,50,50,50);
-  // game.clearRect()
-  // game.clearRect(0,0,50,50);
+      if(col == 'O') {
+        //console.log({posX, posY});
+        // asignamos las posiciones al objeto
+        playerPosition.x = posX;
+        playerPosition.y = posY;
+      }
 
-  // game.font = '25px Verdana'
-  // game.fillStyle = 'purple';
-  // game.textAlign = 'center';
-  // game.fillText('Platzi', 25, 25);
+      game.fillText(emoji, posX, posY);
+      // el siguiente clg imprime las posiciones del jugador
+      //console.log({row, rowI, col, colI});
+    });
+  });
+  // for(let row = 1; row <= 10; row++) {
+  //   for(let col = 1; col <= 10; col++) {
+  //     game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+  //   }
+  // }
+  movePlayer();
 }
+
+function movePlayer() {
+  game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+}
+
+// movimientos del jugador
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+//funcion paraa campturar teclas presionadas
+function moveByKeys(event) {
+  //sintaxis upcional 2
+  if(event.key == 'ArrowUp') moveUp();
+  else if(event.key == 'ArrowLeft') moveLeft();
+  else if(event.key == 'ArrowRight') moveRight();
+  else if(event.key == 'ArrowDown') moveDown();
+
+  // sintaxis opcional 1
+  // if(event.key == 'ArrowUp') {
+  //   moveUp();
+  // } else if(event.key == 'ArrowLeft') {
+  //   moveLeft();
+  // } else if(event.key == 'ArrowRight') {
+  //   moveRight();
+  // } else if(event.key == 'ArrowDown') {
+  //   moveDown();
+  // }
+}
+
+function moveUp() {
+  console.log('Me quiero mover hacia arriba');
+  playerPosition.y -= elementsSize;
+  movePlayer();
+}
+
+function moveLeft() {
+  console.log('Me quero mover hacia la izquierda');
+}
+
+function moveRight() {
+  console.log('Me quiero mover hacia la derecha');
+}
+
+function moveDown() {
+  console.log('Me quiero mover hacia abajo');
+}
+
